efe9783a02d08a53fd44091de2e105f3847459c0
Hamad-Khalil <123974803+Hamad-Khalil@users.noreply.github.com>
2023-02-19 22:14:11 +0500

    Add files via upload

---
+++ 19-2-23/practice.js
@@ -0,0 +1,50 @@
+// let numberOfDay = 9;
+// switch (numberOfDay) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+
+//     default:
+//         console.log("Invalid input");
+//         break;
+// }
+// let a = 7;
+// let i = 1;
+// while(i<=10){
+//     console.log(a +  ' X ' + i + ' = ' + a*i);
+//     i++;
+// }
+// let number= 8;
+// for(let i=1; i<=10; i++)
+// { console.log(number + " X " + i + " = " + number*i);}
+
+// let fruites=["Apple", "Mango", "Orange","Banana"]
+//  fruites.pop()
+// fruites.push("Dates")
+// {console.log(fruites);}
+// let number= [0,1,2,3,4]
+// number.shift()
+// console.log(number);
+let num = [0,1,2,3,4,5,6,7]
+for (let i=0; i<num.length; i++)
+{
+    console.log(num[i]);
+}
