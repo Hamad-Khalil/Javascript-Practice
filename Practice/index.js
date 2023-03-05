78bf0d0c940d1e6cc2aac770ff029754d673404d
Hamad Khalil <123974803+Hamad-Khalil@users.noreply.github.com>
2023-03-06 00:42:40 +0500

    Add files via upload

---
+++ Practice/index.js
@@ -0,0 +1,12 @@
+// forEach Function to find sum
+
+const sales=[9,4,3,6,2,6,6,8,2];
+let sum=0;
+sales.forEach(findsum);
+
+function findsum(num){
+    sum+= num;
+}
+console.log(sum);
+
+
---
+++ Practice/index2.js
@@ -0,0 +1,30 @@
+const persons = [
+    {firstname : "Malcom", lastname: "Reynolds"},
+    {firstname : "Kaylee", lastname: "Frye"},
+    {firstname : "Jayne", lastname: "Cobb"}
+  ];
+  let fullName= persons.map(getFullName)
+  
+
+  console.log(fullName);
+  function getFullName(item) {
+    return [item.firstname,item.lastname].join(" ");
+  }
+
+
+
+
+// let num= [1,2,3,4,5,6,7];
+
+// num.forEach(counting)
+// function counting(item,index){
+//    console.log('index ' + index + ' item ' + item);
+// }
+
+// function counting(elements){
+//     for(let i=0; i<=num.length; i++){
+//         count=num.length
+//     }
+//     return count
+// }
+// console.log(counting(num));
