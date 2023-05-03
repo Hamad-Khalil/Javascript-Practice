8cc0add906d7c414db59b5e36ca7b92ea1623874
Hamad Khalil <123974803+Hamad-Khalil@users.noreply.github.com>
2023-05-03 23:44:18 +0500

    Add files via upload

---
+++ Advance JS/ObjClasses.js
@@ -0,0 +1,26 @@
+// // class person{
+// //     constructor(){
+// //         this.name="Hamad" 
+// //         this.age=20
+// //     }
+// // }
+// // let perObj= new person()
+// // console.log(perObj);
+
+// class stu{
+//     constructor(name, age){
+//         this.UserName= name;
+//         this.userAge= age;
+//     }
+//     About(){
+//         console.log(`${this.UserName} is ${ this.userAge} years old`);
+//     }
+//     static Nationality(){
+//         console.log(UserName);
+//         console.log(userAge);
+//     }
+// }
+// let stuAbout= new stu("Hamad", 20)
+// stuAbout.About()
+// stu.Nationality()
+
--- Practice/index.js
+++ Practice/index.js
@@ -1,12 +1,279 @@
 // forEach Function to find sum
 
-const sales=[9,4,3,6,2,6,6,8,2];
-let sum=0;
-sales.forEach(findsum);
+// const sales=[9,4,3,6,2,6,6,8,2];
+// let sum=0;
+// sales.forEach(findsum);
 
-function findsum(num){
-    sum+= num;
-}
-console.log(sum);
+// function findsum(num){
+//     sum+= num;
+// }
+// console.log(sum);
+
+// code to find number divisible to 5 in array with filter
+// 
+// let array=[12,15,23,25,34,35,45,56,67,75]
+// let divisible5= array.filter(num => num %5===0)
+// console.log(divisib)
+
+
+
+let number=[1,-2,3,-4,5,-6,7,-8,9,-10]
+let negativeNum=number.filter(Element => Element <0);
+console.log(negativeNum.length);
+
+// const persons = [
+//     {firstname : "Malcom", lastname: "Reynolds"},
+//     {firstname : "Kaylee", lastname: "Frye"},
+//     {firstname : "Jayne", lastname: "Cobb"}
+//   ];
+//   let fullName= persons.map(getFullName)
+  
+
+//   console.log(fullName);
+//   function getFullName(item) {
+//     return [item.firstname,item.lastname].join(" ");
+//   }
+
+
+
+
+// let num= [2,3,4,5,6,7];
+
+// num.forEach(counting)
+// function counting(item,index){
+//    console.log('index ' + index + ' item ' + item);
+// }
+
+// function counting(elements){
+//     for(let i=0; i<=num.length; i++){
+//         count=num.length
+//     }
+//     return count
+// }
+// console.log(counting(num));
+
+
+
+// To find even numbers
+// let evenNum= array.filter(item => item % 2 === 0)
+// console.log(evenNum);
+
+
+// let array=[ 1,2,3,4,5]
+
+// array.unshift(0)
+// console.log(array);
+
+// array.shift()
+// console.log(array);
+
+// array.pop()
+// console.log(array);
+
+// array.push(6)// code to find Negative value in array with the help of .forEach 
+
+// const array= [1,2,-3,2,-4,5,-4,6,7,-6]
+// let Negative=[];
+// array.forEach (findNegative);
+// function findNegative(num){
+//    if( num<0){
+//    Negative.push(num)
+//    }
+//    return Negative
+// }
+// console.log(Negative)
+
+// code to separate odd and even Number into differents arrays
+
+// let numbers=[1,2,3,4,5,6,7,8,9,10];
+// let evenNum=[];
+// let oddNum=[]
+// numbers.forEach(sepEvenOdd)
+// function sepEvenOdd(num){
+//     if( num % 2===0){
+//         evenNum.push(num)
+//     } else if( num % 2 != 0){
+//         oddNum.push(num)
+//     }
+// }
+// console.log(evenNum);
+// console.log(oddNum);
+
+// js code to find squra root of the values
+
+// let array=[4,9,16,25,36]
+// let newArr= array.map(Math.sqrt)
+// console.log(newArr);
+
+// let arr=[1,-2,3,-4,5,-6];
+// let negArr=[];
+// for(let i=0;i<arr.length; i++){
+//     if(arr[i]<0){
+//         negArr.push(arr[i])
+//     }
+// }
+
+// console.log(negArr);
+
+// let numberOfDay = 9;
+// switch (numberOfDay) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+
+//     default:
+//         console.log("Invalid input");
+//         break;
+// }
+// let a = 7;
+// let i = 1;
+// while(i<=10){
+//     console.log(a +  ' X ' + i + ' = ' + a*i);
+//     i++;
+// }
+// let number= 8;
+// for(let i=1; i<=10; i++)
+// { console.log(number + " X " + i + " = " + number*i);}
+
+// let fruites=["Apple", "Mango", "Orange","Banana"]
+//  fruites.pop()
+// fruites.push("Dates")
+// {console.log(fruites);}
+// let number= [0,1,2,3,4]
+// number.shift()
+// console.log(number);
+// let num = [0,1,2,3,4,5,6,7]
+// for (let i=0; i<num.length; i++)
+// {
+//     console.log(num[i]);
+// }
+// console.log(array);
+// code to find number divisible to 5 in array with filter
+// 
+// let array=[12,15,23,25,34,35,45,56,67,75]
+// let divisible5= array.filter(num => num %5===0)
+// console.log(divisib)
+
+
+// code to find Negative value in array with the help of .forEach 
+
+// const array= [1,2,-3,2,-4,5,-4,6,7,-6]
+// let Negative=[];
+// array.forEach (findNegative);
+// function findNegative(num){
+//    if( num<0){
+//    Negative.push(num)
+//    }
+//    return Negative
+// }
+// console.log(Negative)
+
+// code to separate odd and even Number into differents arrays
+
+// let numbers=[1,2,3,4,5,6,7,8,9,10];
+// let evenNum=[];
+// let oddNum=[]
+// numbers.forEach(sepEvenOdd)
+// function sepEvenOdd(num){
+//     if( num % 2===0){
+//         evenNum.push(num)
+//     } else if( num % 2 != 0){
+//         oddNum.push(num)
+//     }
+// }
+// console.log(evenNum);
+// console.log(oddNum);
+
+// js code to find squra root of the values
+
+// let array=[4,9,16,25,36]
+// let newArr= array.map(Math.sqrt)
+// console.log(newArr);
+
+// let arr=[1,-2,3,-4,5,-6];
+// let negArr=[];
+// for(let i=0;i<arr.length; i++){
+//     if(arr[i]<0){
+//         negArr.push(arr[i])
+//     }
+// }
+
+// console.log(negArr);
+// let number=[1,-2,3,-4,5,-6,7,-8,9,-10]
+// let negativeNum=number.filter(Element => Element <0);
+// console.log(negativeNum.length);
+
+
+
+
+
+// To find even numbers
+// let evenNum= array.filter(item => item % 2 === 0)
+// console.log(evenNum);
+
+
+// let array=[ 1,2,3,4,5]
+
+// array.unshift(0)
+// console.log(array);
+
+// array.shift()
+// console.log(array);
+
+// array.pop()
+// console.log(array);
+
+// array.push(6)
+// console.log(array);
+
+
+// const persons = [
+//     {firstname : "Malcom", lastname: "Reynolds"},
+//     {firstname : "Kaylee", lastname: "Frye"},
+//     {firstname : "Jayne", lastname: "Cobb"}
+//   ];
+//   let fullName= persons.map(getFullName)
+  
+
+//   console.log(fullName);
+//   function getFullName(item) {
+//     return [item.firstname,item.lastname].join(" ");
+//   }
+
+
+
+
+// let num= [2,3,4,5,6,7];
+
+// num.forEach(counting)
+// function counting(item,index){
+//    console.log('index ' + index + ' item ' + item);
+// }
+
+// function counting(elements){
+//     for(let i=0; i<=num.length; i++){
+//         count=num.length
+//     }
+//     return count
+// }
+// console.log(counting(num));
 
 
