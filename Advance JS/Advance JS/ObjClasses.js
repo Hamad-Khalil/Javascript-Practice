1732021e77112aac3319eadbb9078a770295f8ff
Hamad Khalil <123974803+Hamad-Khalil@users.noreply.github.com>
2023-05-05 23:31:46 +0500

    Add files via upload

---
+++ Advance JS/Advance JS/ObjClasses.js
@@ -0,0 +1,44 @@
+// // class person{
+// //     constructor(){
+// //         this.name="Hamad" 
+// //         this.age=20
+// //     }
+// // }
+// // let perObj= new person()
+// // console.log(perObj);
+
+// class stu{
+//     constructor(name, age){
+//         this.UserName= name;
+//         this.userAge= age;
+//     }
+//     About(){
+//         console.log(`${this.UserName} is ${ this.userAge} years old`);
+//     }
+//     static Nationality(){
+//         console.log(UserName);
+//         console.log(userAge);
+//     }
+// }
+// let stuAbout= new stu("Hamad", 20)
+// stuAbout.About()
+// stu.Nationality()
+
+
+// class person{
+//     constructor(Nam, umer){
+//         this.name= Nam
+//         this.age= umer;
+//     }
+//     about(){
+//         console.log(this.name,"is", this.age,'years old');
+//     }
+//     static Religon(){
+//         console.log(name);
+//         console.log(age);
+//     }
+    
+// }
+// let perObj=new person('Hamad', 20)
+// perObj.about()
+
