ae6b9fdb3755225c1eb7672ce874f9d59aaa1bfa
Hamad Khalil <123974803+Hamad-Khalil@users.noreply.github.com>
2023-07-15 22:07:39 +0500

    Add files via upload

---
+++ Rest Operator/index.js
@@ -0,0 +1,38 @@
+// let arr=[234879,78,49,265,6509]
+// let [ele1, ele2,...rest]= arr
+// console.log(ele1);
+// console.log(ele2);
+// console.log(rest);
+
+
+// let obj = {
+//     Name: 'ali',
+//     City: 'fsd',
+//     Qualification:'BSCA',
+//     Religion: 'Islam',
+//     Nationality: "Pakistani"
+// }
+// let {Name,City, ...rest}= obj
+// console.log(Name);
+// console.log(City);
+// console.log(rest);
+
+// let obj= { 
+//     Math:{
+//         MCQc:10,
+//         SQs:20,
+//     },
+//     Sci: 81,
+//     English:90,
+//     Urdu: 83,
+//     PakStudy:99
+// }
+// let {Math,Sci,...rest}= obj
+// console.log(Math);
+// console.log(Sci);
+// console.log(rest);
+let arr=[1,2,3,4,5,6]
+let arr2=arr.reverse()
+console.log(arr2);
+// let [ele1, ele2,...rest]= arr2
+// console.log(ele1, ele2,...rest);
